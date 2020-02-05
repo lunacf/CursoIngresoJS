639366f859286da546491f8531a1186a719fcfe5
lunacf <60608120+lunacf@users.noreply.github.com>
2020-02-04 21:19:32 -0300

    Add files via upload
    "Ejercicio 7 done"

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,61 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+    var numeroUno;
+    var numeroDos;
+    var resultado; 
+
+    numeroUno = parseInt(document.getElementById("numeroUno"). value);
+    numeroDos = parseInt(document.getElementById("numeroDos"). value);
+    
+    resultado = numeroUno + numeroDos;
+
+    alert("La suma es "+ resultado);
+}
+
+function restar()
+{
+	var numeroUno;
+    var numeroDos;
+    var resultado; 
+
+    numeroUno = parseInt(document.getElementById("numeroUno"). value);
+    numeroDos = parseInt(document.getElementById("numeroDos"). value);
+
+    resultado = numeroUno - numeroDos;
+
+    alert("La resta es "+ resultado);
+
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+    numeroUno = parseInt(document.getElementById("numeroUno"). value);
+    numeroDos = parseInt(document.getElementById("numeroDos"). value);
+
+    resultado = numeroUno * numeroDos;
+
+    alert("La multiplicación es "+ resultado);
+}
+
+function dividir()
+{
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+    numeroUno = parseInt(document.getElementById("numeroUno"). value);
+    numeroDos = parseInt(document.getElementById("numeroDos"). value);
+
+    resultado = numeroUno / numeroDos;
+
+    alert("La división es "+ resultado);
+}
+

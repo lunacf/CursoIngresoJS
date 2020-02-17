5bdadca1174a4020a43c87610a28241d906a0085
lunacf <60608120+lunacf@users.noreply.github.com>
2020-02-17 10:41:51 -0300

    Update jsInstruccionSwitch(1).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+  case 1: "Enero"
+  alert("hola");
+    break;
+    }
+  {
+  case 2: "Marzo"
+  alert("Chau");
+  break;
+  }
 
 
 

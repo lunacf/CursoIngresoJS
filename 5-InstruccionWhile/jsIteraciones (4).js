44e7dcd9c923ba325a51207908e9a7d8caaffe63
lunacf <60608120+lunacf@users.noreply.github.com>
2020-02-18 00:12:19 -0300

    4 y 5

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,14 @@
-function mostrar()
-{
+function mostrar() {
+	     //Declaramos variable, parseamos
+	var numero = parseInt(prompt("ingrese un número entre 0 y 9."));
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	while (numero <0 || numero >9 || isNaN(numero)) {
+		//Copio lo mismo que cuando declare la variable
+		numero = parseInt(prompt("Error. Reingrese un numero"));
+	}
+	    //Declaro document.getElementById y lo igualo a la variable (numero)
+
+	document.getElementById("Numero").value = numero;
 
 
 }//FIN DE LA FUNCIÓN
